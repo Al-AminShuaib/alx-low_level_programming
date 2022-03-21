a531652fa626bf3cc9ef0db6fa3f80bb1ae7f9ad
Al-AminShuaib <dimewalien@gmail.com>
2022-03-21 10:17:54 -0700

    new

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,35 @@
-#include <stdio.h>
 #include "main.h"
+
+#include <stdio.h>
+
+
+
+void main()
+
+{
+
+		char *s = "Hello new\n";
+
+			int x = _strlen(s);
+
+				printf("%d - length\n", x);
+
+}
+
+/**
+ 
+   * _strlen - counts length of a sting or char pointer
+ 
+    * @s: accept user input
+    * Return: always return legnth counted
+ */
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while(* != '\0')
+int count = 0;
+while (*s != '\0')
 	{
+		count++;
 		s++;
-		len++
 	}
-	return(len)
+		return (count);
 }

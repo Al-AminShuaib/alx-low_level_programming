640ef6f13fe828668fe91dd973c32d116bbfa34c
Al-AminShuaib <dimewalien@gmail.com>
2022-03-28 04:39:21 -0700

    new commits

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,32 +4,70 @@
 
 /**
 
- * _strchr - check strings
+ * _strspn - checking first segment for same characters
 
- * @s: strings of letters
+ * @s: string to check
 
- * @c: single to check with
+ * @accept: verify strings are in first byte
 
- * Return: c
+ * Return: int numbers of characters that are the same
 
  */
 
 
 
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
   
 {
   
-  while (*s != c)
+  int x = 0;
+  
+  int i = 0;
+  
+  int cnt = 0;
+  
+
+  
+  while (accept[x] != 0)
     
     {
       
-      if (!*s++)
+    again:
+      
+      if (accept[x] != s[i] && s[i] != 32 && s[i] != 0)
 	
-	return (NULL);
+	{
+	  
+	  i++;
+	  
+	  goto again;
+	  
+	}
+      
+      else if (s[i] == 32 || s[i] == 0)
+	
+	{
+	  
+	  i = 0;
+	  
+	  x++;
+	  
+	  if (accept[x] == 0)
+	    
+	    break;
+	  
+	  goto again;
+	  
+	}
+      
+      else
+	
+	cnt++;
+      
+      i++;
       
     }
   
-  return (s);
+  return (cnt);
   
 }
